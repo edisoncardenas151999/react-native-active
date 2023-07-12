606db6c1346646ddcd0d99615d9259cbd76674b2
edisoncardenas151999 <108204103+edisoncardenas151999@users.noreply.github.com>
2023-07-11 23:04:20 -0400

    Add files via upload

---
+++ rn-starter/src/screens/ComponentsScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+
+const ComponentsScreen = () =>{
+  return <Text style ={styles.textStyle} >ComponentsScreen</Text>
+};
+
+const styles = StyleSheet.create({
+    textStyle:{
+        fontSize: 30
+    }
+})
+export default ComponentsScreen;
